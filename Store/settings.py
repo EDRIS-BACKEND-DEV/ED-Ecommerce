dc9af011de0f5adad05d667a497f20d9bc7a2276
edris-developer <125907234+edris-developer@users.noreply.github.com>
2024-04-30 15:13:13 +0330

    Initial commit

--- Store/settings.py
+++ Store/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e8@ay0m)k)if%)8o+99bn#*u9$lb28)0)vlwnlt(f-h_%fpn36'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
